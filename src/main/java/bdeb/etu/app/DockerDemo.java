cd4a72f28c69778520375a82a6ff02f25d68fa58
benka <benkaddour.w@gmail.com>
2025-04-09 12:19:11 -0400

    Test de webhook

--- src/main/java/bdeb/etu/app/DockerDemo.java
+++ src/main/java/bdeb/etu/app/DockerDemo.java
@@ -3,5 +3,6 @@ package bdeb.etu.app;
 public class DockerDemo {
     public static void main(String[] args) {
         System.out.println("wafaa: Test des images docker");
+        System.out.println("wafaa: Test de webhook");
     }
 }
